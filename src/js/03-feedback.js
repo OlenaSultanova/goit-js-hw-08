b932ebc7883ecb7f21b115aa2d202a3576d744fd
OlenaSultanova <galeodeahelen@gmail.com>
2023-08-18 16:54:14 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,44 +1,37 @@
 import throttle from 'lodash.throttle';
 
-const emailInput = document.querySelector('input[name="email"]');
-const messageInput = document.querySelector('textarea[name="message"]');
-const submitButton = document.querySelector('button[type="submit"]');
-const form = document.querySelector('.feedback-form');
+const LOCAL_KEY = 'feedback-form-state';
 
-const saveFormData = throttle(() => {
-  const formData = {
-    email: emailInput.value,
-    message: messageInput.value,
-  };
-  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
-}, 500);
+form = document.querySelector('.feedback-form');
 
-const loadFormData = () => {
-  const savedFormData = localStorage.getItem('feedback-form-state');
-  if (savedFormData) {
-    const parsedFormData = JSON.parse(savedFormData);
-    emailInput.value = parsedFormData.email;
-    messageInput.value = parsedFormData.message;
+form.addEventListener('input', throttle(onInputData, 500));
+form.addEventListener('submit', onFormSubmit);
+
+let dataForm = JSON.parse(localStorage.getItem(LOCAL_KEY)) || {};
+const { email, message } = form.elements;
+reloadPage();
+
+function onInputData(e) {
+  dataForm = { email: email.value, message: message.value };
+  localStorage.setItem(LOCAL_KEY, JSON.stringify(dataForm));
+}
+
+function reloadPage() {
+  if (dataForm) {
+    email.value = dataForm.email || '';
+    message.value = dataForm.message || '';
   }
-};
+}
 
-const clearFormData = () => {
-  localStorage.removeItem('feedback-form-state');
-  emailInput.value = '';
-  messageInput.value = '';
-};
-
-emailInput.addEventListener('input', saveFormData);
-messageInput.addEventListener('input', saveFormData);
-submitButton.addEventListener('click', clearFormData);
-form.addEventListener('submit', e => {
+function onFormSubmit(e) {
   e.preventDefault();
-  const formData = {
-    email: emailInput.value,
-    message: messageInput.value,
-  };
-  console.log(formData);
-  clearFormData();
-});
+  console.log({ email: email.value, message: message.value });
 
-loadFormData();
+  if (email.value === '' || message.value === '') {
+    return alert('Please fill in all the fields!');
+  }
+
+  localStorage.removeItem(LOCAL_KEY);
+  e.currentTarget.reset();
+  dataForm = {};
+}
